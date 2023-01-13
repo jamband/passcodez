bf9f27b3bb2dfe761f7050ed97851f653ec7bd8c
Tomoki Morita <tmsongbooks215@gmail.com>
2023-01-13 16:29:50 +0900

    remove unnecessary some lines

--- src/hooks/step/index.ts
+++ src/hooks/step/index.ts
@@ -2,8 +2,7 @@ import { useState } from "react";
 import type { Step } from "~/types";
 
 export const useStep = () => {
-  const initialState: Step = "first";
-  const [state, setState] = useState<Step>(initialState);
+  const [state, setState] = useState<Step>("first");
 
   const set = (value: Step) => {
     setState(value);
