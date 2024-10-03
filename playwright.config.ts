17522f9d96a1e70e34a5c2f26277fa320d73b0ca
Tomoki Morita <tmsongbooks215@gmail.com>
2024-10-03 23:50:37 +0900

    use chrome instead of chromium

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
   },
   projects: [
     {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      name: "chrome",
+      use: { ...devices["Desktop Chrome"], channel: "chrome" },
     },
     {
       name: "firefox",
