ea0cd1ad2001c789dd6d64ad4f3f25b260f40031
Tomoki Morita <tmsongbooks215@gmail.com>
2025-01-12 10:42:30 +0900

    use vitest/config instead of vitest for reference types

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vite/client" />
-/// <reference types="vitest" />
+/// <reference types="vitest/config" />
 
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "node:path";
