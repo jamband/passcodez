a2300cf7a0c72521f9ba7d8bd14a3e5554a5d041
Tomoki Morita <tmsongbooks215@gmail.com>
2025-03-22 12:18:36 +0900

    change vite root from src to project root

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,13 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: resolve(__dirname, "src"),
   resolve: {
     alias: { "@": resolve(__dirname, "src") },
-  },
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
   },
   plugins: [react()],
   test: {
     environment: "happy-dom",
-    setupFiles: "../vitest.setup.ts",
-    include: ["**/*.test.{ts,tsx}"],
+    setupFiles: "vitest.setup.ts",
+    include: ["src/**/*.test.{ts,tsx}"],
   },
 });
